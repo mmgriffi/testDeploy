6f2c8426fa556e280e7a751ef0377e8a1b6218e4
mmgriffi <michaelmgriffin82@gmail.com>
2022-12-12 14:14:12 +0000

    Create HC_installSQLdb.ps1

---
+++ HC_installSQLdb.ps1
@@ -0,0 +1,32 @@
+$errorcode = 0
+write-host "Checking installed programs to see if app was installed"
+$Progs = (Get-WmiObject Win32_Product| select Name,Version,identifyingNumber)
+foreach ($prog in $progs) 
+{ 
+$progID = $prog.identifyingNumber
+$progName = $prog.Name
+$progversion = $prog.Version
+     
+    if ($progID -eq "{ABB6AC00-F1D8-4EBF-8128-830D090B76C0}")
+     {
+     #write-host "you have installed $progName, version $progversion"
+     
+     $myprogName = $progName
+     $myprogVersion = $progVersion
+     $installed = $true
+
+     #write-host "installed $progName"
+     }
+}
+if ($installed)
+{
+    write-host "you have installed $myprogName, version $myprogversion"
+}
+else
+{
+    $errorcode = 999
+}
+#===
+
+write-host "this script will exit with $errorcode"
+exit $errorcode
