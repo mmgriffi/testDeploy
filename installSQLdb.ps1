489cdbd9064f625b2eec91aab8e95acd19671fa2
mmgriffi <michaelmgriffin82@gmail.com>
2022-12-13 11:46:58 +0000

    change code to use msi file in repo

--- installSQLdb.ps1
+++ installSQLdb.ps1
@@ -6,14 +6,14 @@ $id = "ABB6AC00-F1D8-4EBF-8128-830D090B76C0"
 
 function install()
 {
-$command = 'msiexec.exe /i "C:\temp\SQL2000SampleDb.msi" targetdir="c:\temp\sql2000" /quiet /qn /norestart'
+$command = 'msiexec.exe /i "SQL2000SampleDb.msi" targetdir="c:\temp\sql2000" /quiet /qn /norestart'
 $command = $command + ' /Lv c:\temp\installer1.log'
 invoke-expression $command 
 }
 
 function uninstall()
 {
-$command = 'msiexec.exe /x  "C:\temp\SQL2000SampleDb.msi" /quiet /qn /norestart '
+$command = 'msiexec.exe /x  "SQL2000SampleDb.msi" /quiet /qn /norestart '
 $command = $command + ' /Lv c:\temp\uninstaller.log'
 #$command | out-file c:\temp\installCommandline.txt
 #& $command
