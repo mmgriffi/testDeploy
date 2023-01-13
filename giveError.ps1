33cc920b66ab55fdbb65d54d022e5c2fbb73781a
mmgriffi <michaelmgriffin82@gmail.com>
2023-01-13 15:20:09 +0000

    Update giveError.ps1

--- giveError.ps1
+++ giveError.ps1
@@ -1,3 +1,17 @@
-$errorcode = 999
+$testfile = "\\192.168.1.20\smb\mmgriffi\CICD\validfile.txt"
+   if (test-path $testfile)
+      {
+       write-host "$testfile was found, we can close the script with a $errorcode (success)"
+       $errorcode = 0
+       }
+
+    else
+    {
+        $errorcode = 999
+        write-host "$testfile was not found, we can close the script with a $errorcode (failure)"
+    }
+
+  
+       
 write-host "We will post an $errorcode on this script no matter what"
 exit $errorcode
