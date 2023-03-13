31c1f1be55a1323c7da44419f0dca5b544242a19
mmgriffi <michaelmgriffin82@gmail.com>
2023-03-13 20:00:38 +0000

    add test-path script

---
+++ test-path.ps1
@@ -0,0 +1,25 @@
+﻿[CmdletBinding()]
+param
+(
+[Parameter(Mandatory= $true)]
+[String]$mypath
+)
+
+$errorcode = 0
+
+"logfile" | Out-File testpathlog.log
+
+test-path $mypath
+
+if (test-path $mypath)
+{
+    $message =  "path was found, $errorcode"
+    $message |  Out-File testpathlog.log -append
+}
+else
+   {
+    $errorcode =1
+    $message =  "path was not found, $errorcode"
+   }
+
+EXIT $errorcode
