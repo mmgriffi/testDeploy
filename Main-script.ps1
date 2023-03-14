fe8564af3e5995730f880e987d199c233b55ebdc
mmgriffi <michaelmgriffin82@gmail.com>
2023-03-14 21:18:28 +0000

    Add files via upload
    update

--- Main-script.ps1
+++ Main-script.ps1
@@ -7,7 +7,9 @@ param(
 
 #$OutputVariable = Invoke-Expression -Command "$CI_PROJECT_DIR\Scripts\NodesList.ps1 -Site $site -CI_PROJECT_DIR $CI_PROJECT_DIR" 
 
-$OutputVariable = @('F32IRD415N1.f32int.mfgint.intel.com', 'F32IAP216N3.f32int.mfgint.intel.com')
+#$OutputVariable = @('F32IRD415N1.f32int.mfgint.intel.com', 'F32IAP216N3.f32int.mfgint.intel.com')
+
+$OutputVariable = @('clvwsrv2016n3','kyvwdc01')
 
 Write-Host "Nodes List from DNS: " $OutputVariable 
 
@@ -30,39 +32,11 @@ $server-contain:
         - cd `$CI_PROJECT_DIR\Scripts
     script:
         - echo 'Begin contain for $server'
-        - powershell `$CI_PROJECT_DIR\Scripts\Contain.ps1 -CI_PROJECT_DIR `$CI_PROJECT_DIR -site $site -LogFile $LogFile -server $server
+        - invoke-command -computername ${server} -scriptblock { dir c:\temp }  
     when: 'manual'
     tags:
         - deploy_mit_$site
 
-$server-deploy_postval:
-    stage: deploy_postval
-    script:
-        - echo 'Begin deploy and Post validation for $server'
-        - powershell `$CI_PROJECT_DIR\Scripts\Deploy.ps1 -CI_PROJECT_DIR `$CI_PROJECT_DIR -site $site -sourcefile $sourcefile -LogFile $LogFile -server $server
-    when: 'manual'
-    #needs: ['$server-contain']
-    tags:
-        - deploy_mit_$site
-
-$server-rollback:
-    stage: rollback
-    script:
-        - echo 'Begin rollback for $server'
-        - powershell `$CI_PROJECT_DIR\Scripts\rollback.ps1 -site $site -sourcefile $sourcefile -server $server -CI_PROJECT_DIR `$CI_PROJECT_DIR
-    when: 'manual'
-    tags:
-        - deploy_mit_$site
-
-$server-uncontain:
-    stage: uncontain
-    script:
-        - echo 'Begin uncontain for $server'
-        - powershell `$CI_PROJECT_DIR\Scripts\uncontain.ps1 -CI_PROJECT_DIR `$CI_PROJECT_DIR -site $site -LogFile $LogFile -server $server
-    when: 'manual'
-    #needs: ['$server-deploy_postval']
-    tags:
-        - deploy_mit_$site
 "
 #this will generate multiple files base on site name
 #$config | Out-File .\dynamic-config\$server-config-ci.yml -Encoding utf8
